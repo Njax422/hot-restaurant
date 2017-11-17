824b4fb5989c1e9f1c2c290cf28c27f57649cf91
Max Wang <maxwellswang@gmail.com>
2017-11-16 19:23:09 -0500

    initial server.js file

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var reservations = [];
+
+// Gives the user specific routes
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/reserve", function(req, res) {
+	res.sendFile(path.join(__dirname, "make.html"));
+});
+
+// Gets input from user for a new reservation and pushes it to the reservations array
+app.post("/new", function(req, res) {
+	var newReservation = req.body;
+	var ReservationName = newReservation.name;
+	reservations.push(newReservation);
+});
+
+
+// App listening on a specific port
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
